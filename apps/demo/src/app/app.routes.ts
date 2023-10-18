2c73eaaffc4458b803837e91a2508e53a7bf2157
Austin Fahsl <austin@fahsl.io>
2023-10-18 14:45:43 -0600

    feat: add demo page

--- apps/demo/src/app/app.routes.ts
+++ apps/demo/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { DemoPageComponent } from './demo-page/demo-page.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'demo',
+  },
+  {
+    path: 'demo',
+    component: DemoPageComponent,
+  },
+];
---
+++ apps/demo/src/app/demo-page/demo-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ng-release-demo-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './demo-page.component.html',
+  styleUrls: ['./demo-page.component.scss'],
+})
+export class DemoPageComponent {}
